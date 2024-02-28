113b2320614daf0220d1091c492eed89c8a2097e
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-02-28 23:25:02 +0000

    change function to method in order to access app properties

--- application/routes.go
+++ application/routes.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 
 	"github.com/erabxes/orders-api-microservices/handler"
+	"github.com/erabxes/orders-api-microservices/repository/order"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
@@ -16,13 +17,17 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
 	router.Get("/{id}", orderHandler.GetById)
