9d5d46e4f5cc50f19e2b1147dc4fb408a9582bb5
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-02-29 19:34:24 +0000

    implement create order function

--- handler/order.go
+++ handler/order.go
@@ -1,9 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
+
+	"github.com/erabxes/orders-api-microservices/model"
 	"github.com/erabxes/orders-api-microservices/repository/order"
 )
 
@@ -11,22 +17,56 @@ type Order struct {
 	Repo *order.RedisRepo
 }
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create an order")
+func (h *Order) Create(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineIten `json:"line_items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		LineItems:  body.LineItems,
+		CreatedAt:  &now,
+	}
+
+	err := h.Repo.Insert(r.Context(), order)
+	if err != nil {
+		fmt.Println("failed to insert:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// finally return the order generated to the client
+	res, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(res))
+	w.WriteHeader(http.StatusCreated)
 }
 
-func (o *Order) List(w http.ResponseWriter, r *http.Request) {
+func (h *Order) List(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("List all orders")
 }
 
-func (o *Order) GetById(w http.ResponseWriter, r *http.Request) {
+func (h *Order) GetById(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get an order by ID")
 }
 
-func (o *Order) UpdateById(w http.ResponseWriter, r *http.Request) {
+func (h *Order) UpdateById(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Update an order by ID")
 }
 
-func (o *Order) DeleteById(w http.ResponseWriter, r *http.Request) {
+func (h *Order) DeleteById(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Delete an order by ID")
 }
